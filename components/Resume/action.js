0e73dedc32ab2ad735517777c09ee56a18839076
ZAIN ALI <97796695+mrZAIN-ALI@users.noreply.github.com>
2025-09-19 15:11:54 +0500

    creatted action

---
+++ components/Resume/action.js
@@ -0,0 +1,5 @@
+export const handleResumeButtonClick = () => {
+    // Replace with your actual resume file path or URL
+    const resumeUrl = '/resume.pdf';
+    window.open(resumeUrl, '_blank');
+};
